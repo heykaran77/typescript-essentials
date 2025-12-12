0faf15e78eec744f0965505d29cdead14e331ca8
Karan Singh <devkaran.codes@gmail.com>
2025-12-13 00:39:23 +0530

    generics

---
+++ dist/generics.js
@@ -0,0 +1,12 @@
+"use strict";
+// generics
+// generic functions
+// generic interfaces
+// generic classess
+Object.defineProperty(exports, "__esModule", { value: true });
+// Generic functions
+function log(item) {
+    console.log(item);
+}
+log("Hello world!");
+//# sourceMappingURL=generics.js.map
---
+++ src/generics.ts
@@ -0,0 +1,14 @@
+// generics
+// generic functions
+// generic interfaces
+// generic classess
+
+// Generic functions
+function log<T>(item: T) {
+  console.log(item);
+}
+
+log<string>("Hello world!");
+
+// Generic interface
+
