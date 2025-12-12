2688fe34f7ca9d13b9ef5d68bc3b2196ee47b8b4
Karan Singh <devkaran.codes@gmail.com>
2025-12-13 01:03:31 +0530

    Generic classess

--- dist/generics.js
+++ dist/generics.js
@@ -10,7 +10,27 @@ function log(item) {
 }
 log("Hello world!");
 function CarDets(obj) {
-    log(JSON.stringify(obj));
+    //   log(JSON.stringify(obj));
+    log(obj);
 }
 CarDets({ name: "Rubicon", model: "Jeep", key: "abce" });
+// Generic Classess
+class Coffee {
+    name;
+    constructor(name) {
+        this.name = name;
+    }
+}
+const c1 = new Coffee("espresso");
+const c2 = new Coffee(32);
+console.log(c1.name, c2.name);
+function abcd(a, b) {
+    return a; // No issues
+    // Error cause TS treats "Directly written string" as Literals NOT strings
+    //   return "hey";
+    // Can do the same by type assertion, no errors
+    //   return "hey" as T;
+    // or  return <T>"hey";
+}
+abcd("hello", "world");
 //# sourceMappingURL=generics.js.map
--- src/generics.ts
+++ src/generics.ts
@@ -25,3 +25,20 @@ function CarDets(obj: Car<string>) {
 CarDets({ name: "Rubicon", model: "Jeep", key: "abce" });
 
 // Generic Classess
+class Coffee<T> {
+  constructor(public name: T) {}
+}
+
+const c1 = new Coffee<string>("espresso");
+const c2 = new Coffee<number>(32);
+console.log(c1.name, c2.name);
+
+function abcd<T>(a: T, b: T): T {
+  return a; // No issues
+  // Error cause TS treats "Directly written string" as Literals NOT strings
+  //   return "hey";
+  // Can do the same by type assertion, no errors
+  //   return "hey" as T;
+  // or  return <T>"hey";
+}
+abcd<string>("hello", "world");
